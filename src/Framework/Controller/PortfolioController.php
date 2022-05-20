b5faf05282a30be747a816553f1470f937d2d13e
robert <robert.saternus@gmail.com>
2022-05-20 11:41:57 +0200

    Introduce basic app layer structure

--- src/Framework/Controller/PortfolioController.php
+++ src/Framework/Controller/PortfolioController.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace App\Framework\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
