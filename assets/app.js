d0ef8eb255e00c9a163844865c5ed511a7db8cf3
robert <robert.saternus@gmail.com>
2022-05-23 11:18:45 +0200

    Add and update assets - css and js files

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,81 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+// //STICKY MENU
+//
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 1){
+//         $('#header-top').addClass("sticky");
+//     }
+//     else{
+//         $('#header-top').removeClass("sticky");
+//     }
+// });
+//
+//
+// //Main function
+//
+// $(document).ready(function () {
+//
+//
+//
+//     //PREELOADER
+//
+//     Pace.on("done", function(){
+//         $(".cover").fadeOut(2000);
+//     });
+//
+//
+//     // OSKRYPTOWANIE DLA MENU
+//     var $height = $(window).height();
+//
+//     $("#header-top .traggle-menu").css("height", $height);
+//
+//     $("#header-top .burger").click(function () {
+//         $(".traggle-menu").show("slide", { direction: "left" }, 500);
+//     });
+//
+//     $(".close").click(function () {
+//         $(".traggle-menu").hide("slide", "right", 500);
+//     });
+//
+//     // //SCRIPTS FOR PROJECTS SUBPAGES
+//     // if($('body').hasClass('projekty')){
+//     //     $('select').niceSelect();
+//     // }
+//
+//     $('a[href^="#"]').on('click', function(event) {
+//         var target = $(this.getAttribute('href'));
+//         if( target.length ) {
+//             event.preventDefault();
+//             $('html, body').stop().animate({
+//                 scrollTop: target.offset().top
+//             }, 1000);
+//         }
+//     });
+//
+//     var width = $(window).width();
+//     if (width <= 767) {
+//         $("#cv .timeline li").removeClass("right").addClass("left")
+//     }
+//
+//
+// });
+//
+//
+// //PODMIANA KLASY PRZY SZEROKOŚCI OKNA 620PX DLA SEKCJI KATEGORIE
+// $(window).resize(function () {
+//
+//     var width = $(window).width();
+//     if (width <= 767) {
+//         $("#cv .timeline li").removeClass("right").addClass("left")
+//     }
+//
+// });
+//
+
+
+
+
